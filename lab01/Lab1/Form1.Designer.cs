c861c62c3f18e2b274bbb9dfabc79c3d1663f061
DesFerr <roma.savchuk.06@mail.ru>
2026-02-18 18:36:12 +0700

    Сделана первая лабораторная.

---
+++ lab01/Lab1/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            numericUpDown1 = new NumericUpDown();
+            numericUpDown2 = new NumericUpDown();
+            numericUpDown3 = new NumericUpDown();
+            textBox1 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)chart1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown3).BeginInit();
+            SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.AlignmentOrientation = System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Vertical | System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Horizontal;
+            chartArea1.AxisX.LabelStyle.Format = "F2";
+            chartArea1.AxisY.LabelStyle.Format = "F2";
+            chartArea1.Name = "ChartArea1";
+            chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            chart1.Legends.Add(legend1);
+            chart1.Location = new Point(12, 95);
+            chart1.Name = "chart1";
+            chart1.Size = new Size(689, 454);
+            chart1.TabIndex = 0;
+            chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15F);
+            label1.Location = new Point(12, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(137, 28);
+            label1.TabIndex = 4;
+            label1.Text = "Скорость, м/c";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15F);
+            label2.Location = new Point(162, 21);
+            label2.Name = "label2";
+            label2.Size = new Size(137, 28);
+            label2.TabIndex = 5;
+            label2.Text = "Угол, градусы";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 15F);
+            label3.Location = new Point(312, 21);
+            label3.Name = "label3";
+            label3.Size = new Size(67, 28);
+            label3.TabIndex = 6;
+            label3.Text = "Шаг, с";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(560, 32);
+            button1.Name = "button1";
+            button1.Size = new Size(141, 41);
+            button1.TabIndex = 7;
+            button1.Text = "Запуск";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(731, 32);
+            button2.Name = "button2";
+            button2.Size = new Size(141, 41);
+            button2.TabIndex = 8;
+            button2.Text = "Очистка";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.DecimalPlaces = 1;
+            numericUpDown1.Increment = new decimal(new int[] { 1, 0, 0, 65536 });
+            numericUpDown1.Location = new Point(12, 50);
+            numericUpDown1.Minimum = new decimal(new int[] { 1, 0, 0, 65536 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(120, 23);
+            numericUpDown1.TabIndex = 9;
+            numericUpDown1.Value = new decimal(new int[] { 2, 0, 0, 0 });
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.DecimalPlaces = 1;
+            numericUpDown2.Increment = new decimal(new int[] { 1, 0, 0, 65536 });
+            numericUpDown2.Location = new Point(162, 50);
+            numericUpDown2.Maximum = new decimal(new int[] { 90, 0, 0, 0 });
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new Size(120, 23);
+            numericUpDown2.TabIndex = 10;
+            numericUpDown2.Value = new decimal(new int[] { 45, 0, 0, 0 });
+            // 
+            // numericUpDown3
+            // 
+            numericUpDown3.DecimalPlaces = 4;
+            numericUpDown3.Increment = new decimal(new int[] { 1, 0, 0, 262144 });
+            numericUpDown3.Location = new Point(312, 50);
+            numericUpDown3.Minimum = new decimal(new int[] { 1, 0, 0, 262144 });
+            numericUpDown3.Name = "numericUpDown3";
+            numericUpDown3.Size = new Size(120, 23);
+            numericUpDown3.TabIndex = 11;
+            numericUpDown3.Value = new decimal(new int[] { 1, 0, 0, 65536 });
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(707, 95);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(165, 454);
+            textBox1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(884, 561);
+            Controls.Add(textBox1);
+            Controls.Add(numericUpDown3);
+            Controls.Add(numericUpDown2);
+            Controls.Add(numericUpDown1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(chart1);
+            MaximizeBox = false;
+            MaximumSize = new Size(900, 600);
+            MinimumSize = new Size(900, 600);
+            Name = "Form1";
+            Text = "Lab1";
+            ((System.ComponentModel.ISupportInitialize)chart1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown3).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button button1;
+        private Button button2;
+        private NumericUpDown numericUpDown1;
+        private NumericUpDown numericUpDown2;
+        private NumericUpDown numericUpDown3;
+        private TextBox textBox1;
+    }
+}
---
+++ lab01/Lab1/Form1.cs
@@ -0,0 +1,80 @@
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        int series_amount = 0;
+        private List<Color> colors = new List<Color>
+        {
+            Color.Red,
+            Color.Green,
+            Color.Blue,
+            Color.Orange,
+            Color.Purple,
+            Color.Brown,
+            Color.Pink,
+            Color.Cyan,
+            Color.Black
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            series_amount += 1;
+            const double g = 9.8;
+            double x = 0, y = 0, v0 = 0, alpha = 0, t_step = 0, t = 0, vx = 0, vy = 0, max_height = 0;
+            v0 = (double)numericUpDown1.Value;
+            alpha = (double)numericUpDown2.Value;
+            t_step = (double)numericUpDown3.Value;
+            string series_name = "График_" + series_amount.ToString();
+            textBox1.AppendText($"{series_name}." + Environment.NewLine);
+            textBox1.AppendText("Параметры:" + Environment.NewLine);
+            textBox1.AppendText($"Начальная скорость: {v0} м/c," + Environment.NewLine);
+            textBox1.AppendText($"Угол начала полёта: {alpha}°," + Environment.NewLine);
+            textBox1.AppendText($"Шаг моделирования: {t_step} с." + Environment.NewLine);
+            chart1.Series.Add(series_name);
+            chart1.Series[series_name].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart1.Series[series_name].Color = colors[(series_amount - 1) % 9];
+            chart1.Series[series_name].Points.AddXY(x, y);
+            alpha *= Math.PI / 180;
+            do
+            {
+                t += t_step;
+                x = v0 * Math.Cos(alpha) * t;
+                y = v0 * Math.Sin(alpha) * t - ((g * t * t) / 2);
+                if (y > max_height) { max_height = y; }
+                chart1.Series[series_name].Points.AddXY(x, y);
+                vx = v0 * Math.Cos(alpha);
+                vy = v0 * Math.Sin(alpha) - g * t;
+            } while (y >= 0);
+            textBox1.AppendText("Результаты:" + Environment.NewLine);
+            textBox1.AppendText($"Дальность полёта: {Math.Round(x, 3)} м," + Environment.NewLine);
+            textBox1.AppendText($"Максимальная высота: {Math.Round(max_height, 3)} м," + Environment.NewLine);
+            textBox1.AppendText($"Скорость в конечной точке: {Math.Round(Math.Sqrt((vx * vx) + (vy * vy)), 3)} м/c." + Environment.NewLine);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string series_name;
+            for (int i = 1; i <= series_amount; i++) {
+                series_name = "График_" + i.ToString();
+                chart1.Series.Remove(chart1.Series[series_name]);
+            }
+            series_amount = 0;
+            textBox1.Clear();
+        }
+    }
+}

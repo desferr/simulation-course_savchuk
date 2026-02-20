fd3f71ce22f373bfeaf9a1973fb34cc6311df49c
Савчук Роман Сергеевич <st23.92@CSI.LOCAL>
2026-02-20 10:12:59 +0700

    Исправлены ошибки

--- lab01/Lab1/Form1.Designer.cs
+++ lab01/Lab1/Form1.Designer.cs
@@ -40,10 +40,19 @@
             numericUpDown2 = new NumericUpDown();
             numericUpDown3 = new NumericUpDown();
             textBox1 = new TextBox();
+            numericUpDown4 = new NumericUpDown();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            numericUpDown5 = new NumericUpDown();
+            numericUpDown6 = new NumericUpDown();
             ((System.ComponentModel.ISupportInitialize)chart1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDown3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown6).BeginInit();
             SuspendLayout();
             // 
             // chart1
@@ -55,9 +64,9 @@
             chart1.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
             chart1.Legends.Add(legend1);
-            chart1.Location = new Point(12, 95);
+            chart1.Location = new Point(12, 165);
             chart1.Name = "chart1";
-            chart1.Size = new Size(689, 454);
+            chart1.Size = new Size(689, 384);
             chart1.TabIndex = 0;
             chart1.Text = "chart1";
             // 
@@ -148,17 +157,88 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(707, 95);
+            textBox1.Location = new Point(707, 165);
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(165, 454);
+            textBox1.Size = new Size(165, 384);
             textBox1.TabIndex = 12;
+            // 
+            // numericUpDown4
+            // 
+            numericUpDown4.DecimalPlaces = 1;
+            numericUpDown4.Increment = new decimal(new int[] { 1, 0, 0, 65536 });
+            numericUpDown4.Location = new Point(12, 116);
+            numericUpDown4.Minimum = new decimal(new int[] { 1, 0, 0, 65536 });
+            numericUpDown4.Name = "numericUpDown4";
+            numericUpDown4.Size = new Size(120, 23);
+            numericUpDown4.TabIndex = 13;
+            numericUpDown4.Value = new decimal(new int[] { 5, 0, 0, 0 });
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 15F);
+            label4.Location = new Point(12, 87);
+            label4.Name = "label4";
+            label4.Size = new Size(95, 28);
+            label4.TabIndex = 14;
+            label4.Text = "Масса, кг";
+            label4.Click += label4_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 15F);
+            label5.Location = new Point(162, 87);
+            label5.Name = "label5";
+            label5.Size = new Size(137, 28);
+            label5.TabIndex = 15;
+            label5.Text = "Сечение, м^2";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 15F);
+            label6.Location = new Point(312, 87);
+            label6.Name = "label6";
+            label6.Size = new Size(77, 28);
+            label6.TabIndex = 16;
+            label6.Text = "Коэфф.";
+            // 
+            // numericUpDown5
+            // 
+            numericUpDown5.DecimalPlaces = 1;
+            numericUpDown5.Increment = new decimal(new int[] { 1, 0, 0, 65536 });
+            numericUpDown5.Location = new Point(162, 116);
+            numericUpDown5.Maximum = new decimal(new int[] { 1000, 0, 0, 0 });
+            numericUpDown5.Minimum = new decimal(new int[] { 1, 0, 0, 65536 });
+            numericUpDown5.Name = "numericUpDown5";
+            numericUpDown5.Size = new Size(120, 23);
+            numericUpDown5.TabIndex = 17;
+            numericUpDown5.Value = new decimal(new int[] { 10, 0, 0, 0 });
+            // 
+            // numericUpDown6
+            // 
+            numericUpDown6.DecimalPlaces = 2;
+            numericUpDown6.Increment = new decimal(new int[] { 1, 0, 0, 131072 });
+            numericUpDown6.Location = new Point(312, 116);
+            numericUpDown6.Maximum = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDown6.Name = "numericUpDown6";
+            numericUpDown6.Size = new Size(120, 23);
+            numericUpDown6.TabIndex = 18;
+            numericUpDown6.Value = new decimal(new int[] { 15, 0, 0, 131072 });
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(884, 561);
+            Controls.Add(numericUpDown6);
+            Controls.Add(numericUpDown5);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(numericUpDown4);
             Controls.Add(textBox1);
             Controls.Add(numericUpDown3);
             Controls.Add(numericUpDown2);
@@ -178,6 +258,9 @@
             ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDown3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown6).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -194,5 +277,11 @@
         private NumericUpDown numericUpDown2;
         private NumericUpDown numericUpDown3;
         private TextBox textBox1;
+        private NumericUpDown numericUpDown4;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private NumericUpDown numericUpDown5;
+        private NumericUpDown numericUpDown6;
     }
 }
--- lab01/Lab1/Form1.cs
+++ lab01/Lab1/Form1.cs
@@ -35,10 +35,13 @@ namespace Lab1
         {
             series_amount += 1;
             const double g = 9.8;
-            double x = 0, y = 0, v0 = 0, alpha = 0, t_step = 0, t = 0, vx = 0, vy = 0, max_height = 0;
+            double x = 0, y = 0, v0 = 0, alpha = 0, t_step = 0, t = 0, vx = 0, vy = 0, max_height = 0, m = 0, S = 0, C = 0, ro = 1.29;
             v0 = (double)numericUpDown1.Value;
             alpha = (double)numericUpDown2.Value;
             t_step = (double)numericUpDown3.Value;
+            m = (double)numericUpDown4.Value;
+            S = (double)numericUpDown5.Value;
+            C = (double)numericUpDown6.Value;
             string series_name = "Симуляция_" + series_amount.ToString();
             textBox1.AppendText($"{series_name}." + Environment.NewLine);
             textBox1.AppendText("Параметры:" + Environment.NewLine);
@@ -50,31 +53,42 @@ namespace Lab1
             chart1.Series[series_name].Color = colors[(series_amount - 1) % 9];
             chart1.Series[series_name].Points.AddXY(x, y);
             alpha *= Math.PI / 180;
+            double k = C * S * ro / (2 * m);
+            vx = v0 * Math.Cos(alpha);
+            vy = v0 * Math.Sin(alpha);
+            double v = Math.Sqrt((vx * vx) + (vy * vy));
             do
             {
                 t += t_step;
-                x = v0 * Math.Cos(alpha) * t;
-                y = v0 * Math.Sin(alpha) * t - ((g * t * t) / 2);
+                vx = vx - k * vx * v * t_step;
+                vy = vy - (g + k * vy * v) * t_step;
+                v = Math.Sqrt((vx * vx) + (vy * vy));
+                x = x + vx * t_step;
+                y = y + vy * t_step;
                 if (y > max_height) { max_height = y; }
                 chart1.Series[series_name].Points.AddXY(x, y);
             } while (y >= 0);
-            vx = v0 * Math.Cos(alpha);
-            vy = v0 * Math.Sin(alpha) - g * t;
             textBox1.AppendText("Результаты:" + Environment.NewLine);
             textBox1.AppendText($"Дальность полёта: {Math.Round(x, 3)} м," + Environment.NewLine);
             textBox1.AppendText($"Максимальная высота: {Math.Round(max_height, 3)} м," + Environment.NewLine);
-            textBox1.AppendText($"Скорость в конечной точке: {Math.Round(Math.Sqrt((vx * vx) + (vy * vy)), 3)} м/c." + Environment.NewLine);
+            textBox1.AppendText($"Скорость в конечной точке: {Math.Round(v, 3)} м/c." + Environment.NewLine);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             string series_name;
-            for (int i = 1; i <= series_amount; i++) {
+            for (int i = 1; i <= series_amount; i++)
+            {
                 series_name = "Симуляция_" + i.ToString();
                 chart1.Series.Remove(chart1.Series[series_name]);
             }
             series_amount = 0;
             textBox1.Clear();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
